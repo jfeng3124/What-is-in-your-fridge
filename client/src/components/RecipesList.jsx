b43abccbc7aff70cce39e505aa54a689abb034b6
Jia <jiafeng3124@gmail.com>
2019-05-07 13:46:53 -0700

    formatting

--- client/src/components/RecipesList.jsx
+++ client/src/components/RecipesList.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import RecipeItem from './RecipeItem.jsx';
 
 const List = (props) => (
-  <div>
-    <h4> Recipes </h4>
-    {props.recipes.map(recipe => <RecipeItem recipe={recipe} key={recipe.id} />)}
+  <div className='recipe-main'>
+    <div className='recipe-list'>
+      {props.recipes.map(recipe => <RecipeItem recipe={recipe} key={recipe.id} />)}
+    </div>
   </div>
 )
 
