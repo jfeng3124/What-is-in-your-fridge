9441bdd62797593b28c2a1164566e03bc85b6212
Jia <jiafeng3124@gmail.com>
2019-05-06 21:07:03 -0700

    move functions to review item

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import RecipesList from './components/RecipesList.jsx';
+// import Recipe from './components/Recipe.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -12,6 +13,7 @@ class App extends React.Component {
     }
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+    // this.onClick = this.onClick.bind(this);
   }
 
   onChange(e) {
@@ -30,21 +32,34 @@ class App extends React.Component {
     axios.get('/api/recipes')
       .then(response => {
         this.setState({ recipes: response.data})
-        console.log('response', this.state.recipes)
       })
       .catch(err => console.log(err))
   }
+
+  // getRecipeInfo() {
+  //   axios.get('/api/recipe')
+  //     .then(response => {
+  //       this.setState({ recipe: response.data })
+  //       console.log('response', this.state.recipe)
+  //     })
+  // }
+  
+  // onClick(e) {
+  //   e.preventDefault();
+  //   this.getRecipeInfo();
+  // }
 
   onSubmit(e) {
     e.preventDefault();
     this.sendData();
     this.getData();
+    console.log(this.state)
   }
 
   render() {
     if (!this.state.recipes) {
       return (
-        <div id='on-load'>
+        <div id='main-page'>
           <h1>What's for Dinner?</h1>
             <form onSubmit={this.onSubmit}>
               Please separate ingredients by commas (example: apple,sugar,cinnamon)
@@ -54,10 +69,18 @@ class App extends React.Component {
         </div>
       )
     }
+    // if (!this.state.recipe) {
+    //   return (
+    //     <div>
+    //     <h1>What's for Dinner?</h1>
+    //     <Recipe recipe={this.state.recipe}/>
+    //   </div>
+    //   )
+    // }
     return (
     <div>
       <h1>What's for Dinner?</h1>
-      <RecipesList recipes={this.state.recipes}/>
+      <RecipesList recipes={this.state.recipes} />
     </div>)
   }
 }
