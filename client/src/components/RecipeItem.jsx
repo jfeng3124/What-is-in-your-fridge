7e23b2574b9b34b51eb0d2b751e5fbe5ea11cde3
Jia <jiafeng3124@gmail.com>
2019-05-06 21:06:35 -0700

    change stateless to stateful

--- client/src/components/RecipeItem.jsx
+++ client/src/components/RecipeItem.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
+import axios from 'axios';
+import Recipe from './Recipe';
 
-const RecipeItem = (props) => (
-  <div>
-    <div id='recipe-title'>{ props.item.title }</div>
-    <img id='recipe-photo' src={ props.item.image } />
-  </div>
-)
+class RecipeItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: this.props.item.id,
+      recipe: null
+    }
+  }
+
+  getRecipeInfo(e) {
+    e.preventDefault();
+    axios.get(`/api/recipe/${this.state.id}`)
+      .then(response => {
+        this.setState({ recipe: response.data })
+      })
+  }
+
+  render() {
+    const { recipe } = this.state;
+    if (!recipe) {
+      return (
+        <div>
+          <div id='recipe-title' onClick={this.getRecipeInfo.bind(this)}>{ this.props.item.title }</div>
+          <img id='recipe-photo' src={ this.props.item.image }  />
+        </div>
+      )
+    }
+    return (
+      <div>
+        <Recipe recipe={recipe} key={recipe.id} />
+      </div>
+    )
+  }
+}
+
+// const RecipeItem = (props) => (
+//   <div>
+//     <div id='recipe-title' onClick={(e) => props.onClick(e.target.value)}>{ props.item.title }</div>
+//     <img id='recipe-photo' src={ props.item.image } onClick={(e) => props.onClick(e.target.value)} />
+//   </div>
+// )
 
 export default RecipeItem;
